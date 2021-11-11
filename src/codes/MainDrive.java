460e8875fb40250c36a07f496999c0ba4db56b36
김민성 <kmsz5813@naver.com>
2021-11-11 12:40:31 +0900

    숫자의 범위 검사 진행
     - 1~45까지 사이가 맞는지? 질문 결과 변수에 저장
     - 그 결과가? true가 맞나? 별도 질문

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -5,44 +5,45 @@ import java.util.Scanner;
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
+
 		Scanner myScanner = new Scanner(System.in);
-		
+
 //		입력한 숫자 6개를 저장할 배열
 		int[] myInputNumbers = new int[6];
-		
-		for (int i = 0 ; i < myInputNumbers.length ; i++) {
-			
-			
+
+		for (int i = 0; i < myInputNumbers.length; i++) {
+
 //			각 자리에 맞는 수자를 -> 올바른 숫자를 넣을때 까지 다시 입력
 			while (true) {
-				
+
 //				안내 문구 -> 1번째 숫자 입력 : , 2번째 숫자 입력 : ...
-				System.out.print(i+1 + "번쨰 숫자 입력 : ");
+				System.out.print(i + 1 + "번쨰 숫자 입력 : ");
 				int inputNum = myScanner.nextInt();
-				
+
 //				제약조건 통과했는지? 검사
-				
-				
-//				임시로 무조건 조건 통과라고 명시.
-				if(true) {
-					
+
+//				검사 1. 1 ~ 45 범위 맞는가? 결과를 boolean으로 저장.
+
+				boolean isRangOk = (1 <= inputNum) && (inputNum <= 45);
+
+//				임시로 범위 검사만 진행
+				if (isRangOk) {
+
 //					써도 되는 숫자를 입력 했다.
 					myInputNumbers[i] = inputNum;
-					
+
 //					다음 숫자 받으러 가자. -> while 무한반복 종료.
 					break;
-					
+
 				}
-				
-				
+				else {
+					System.out.println("잘못된 숫자를 입력했습니다. 다시 입력해주세요.");
+				}
+
 			}
-			
-			
-			
-			
+
 		}
-				
+
 	}
-	
+
 }
